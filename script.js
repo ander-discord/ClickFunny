05a87a7bf9c5c1a49f27292f66a51e6c0bffdfad
Im random <fabiosilvatabom1975@gmail.com>
2025-04-12 14:26:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const counter = document.getElementById("counter");
 const clickBtn = document.getElementById("clickBtn");
 const deleteBtn = document.getElementById("deleteBtn");
+const SystemLabel = document.getElementById("SystemLabel");
+const SystelTitle = document.getElementById("SystelTitle");
+
 const ws = new WebSocket("wss://testserver-production-0d2a.up.railway.app");
 
+let last_joke = "No joke.";
 let accountToken = localStorage.getItem("accountToken");
 
 ws.addEventListener("open", () => {
@@ -56,7 +60,7 @@ function renderDigits(value) {
   const digitCount = value.toString().length;
   const target = 10 ** digitCount - 1;
 
-  const percent = Math.min((value / target) * 100, 100);
+  const percent = Math.min((value / target) * 95, 100);
   document.getElementById("progressOverlay").style.height = `${percent}%`;
 
   while (counter.children.length < padded.length) {
@@ -84,7 +88,30 @@ ws.addEventListener("message", event => {
 
   if (data.type === "update") {
     renderDigits(data.count);
-  }
+    document.getElementById("Lastclick").textContent = `Last click From ${data.from}`;
+
+    if (data.joke.type === 'single') {
+      document.getElementById("SystemLabel").textContent = data.joke.joke;
+    } else {
+      document.getElementById("SystemLabel").textContent = `${data.joke.setup} - ${data.joke.delivery}`;
+    }
+
+    if (data.joke.joke !== last_joke) {
+      const now = new Date();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      const ampm = hours >= 12 ? 'PM' : 'AM';
+    
+      hours = hours % 12;
+      hours = hours ? hours : 12; 
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+    
+      SystelTitle.textContent = `System ● ${hours}:${minutes} ${ampm} | Click have chance`;
+      
+      last_joke = data.joke.joke;
+    }
+    
+  }  
 
   if (data.type === "account_created") {
     alert("Account created!");
@@ -101,7 +128,6 @@ ws.addEventListener("message", event => {
     console.log("box2 created:", box2);
   
     const h1 = document.createElement("h1");
-    h1.innerHTML = data.token.replace(/\n/g, "<br>");
     box2.appendChild(h1);
   
     document.body.appendChild(box2);
